91ee0639f310beaca38dd2f9a8626d5ae4f9fa34
yasirghouri <yasir.ag7@gmail.com>
2022-06-07 23:53:15 +0500

    minor change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">React Admin Dashboard</div>;
+  return <div className="App">Welcome To React Admin Dashboard</div>;
 }
 
 export default App;
