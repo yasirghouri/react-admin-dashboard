b9e40f71e6a18e06fdbc5b8b4b1186a33073c9a6
yasirghouri <yasir.ag7@gmail.com>
2022-06-14 21:58:24 +0500

    finished styling of featured and chart component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,3 +1,5 @@
+import Chart from "../../components/Chart/Chart"
+import Featured from "../../components/Featured/Featured"
 import Navbar from "../../components/Navbar/Navbar"
 import Sidebar from "../../components/Sidebar/Sidebar"
 import Widget from "../../components/Widget/Widget"
@@ -6,16 +8,20 @@ import "./Home.scss"
 const Home = () => {
   return (
     <div className="home">
-        <Sidebar />
-        <div className="homeContainer">
-          <Navbar />
-          <div className="widgets">
+      <Sidebar />
+      <div className="homeContainer">
+        <Navbar />
+        <div className="widgets">
           <Widget type="user" />
           <Widget type="order" />
           <Widget type="earning" />
           <Widget type="balance" />
         </div>
+        <div className="charts">
+          <Featured />
+          <Chart title="Last 6 Months (Revenue)" aspect={2 / 1} />
         </div>
+      </div>
     </div>
   )
 }
